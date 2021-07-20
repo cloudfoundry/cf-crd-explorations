0a411ba600d0b220aac36f9b470c67bf627aa812
Tim Downey <tdowney@vmware.com>
2021-07-20 14:34:47 -0700

    Fix ytt incantation hack installation script
    - We needed a placeholder data values file for this to work
    
    Co-authored-by: Tim Downey <tdowney@vmware.com>
    Co-authored-by: Andrew Wittrock <awittrock@vmware.com>

--- hack/install-dependencies.sh
+++ hack/install-dependencies.sh
@@ -96,7 +96,7 @@ hack/generate_certs.sh
 kubectl create secret generic app-validation-webhook -n default --from-file=key.pem=certs/app-validation-webhook-key.pem --from-file=cert.pem=certs/app-validation-webhook-crt.pem
 
 # kubectl apply the webhook files but with ytt subbing out values
-kubectl apply -f <(ytt -f config/webhook/app-validation.yaml --data-value "webhook_ca_cert=$(base64 -w 0 certs/ca.crt )")
+kubectl apply -f <(ytt -f hack/values.yml -f config/webhook/app-validation.yaml --data-value "webhook_ca_cert=$(base64 -w 0 certs/ca.crt )")
 kubectl apply -f config/webhook/rbac.yaml
 
 echo "******************************"
