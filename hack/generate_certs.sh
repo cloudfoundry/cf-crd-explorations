6b681ebc51411c2fe9c13e1cc45aa62125252786
Andrew Wittrock <awittrock@vmware.com>
2021-07-26 16:56:25 -0700

    OpenSSL generation now adds SANs to crt.

--- hack/generate_certs.sh
+++ hack/generate_certs.sh
@@ -8,13 +8,13 @@ set -o pipefail
 openssl genrsa -out certs/ca.key 2048
 
 # GENERATE A CA CERT WITH THE PRIVATE KEY
-openssl req -new -x509 -key certs/ca.key -out certs/ca.crt -config certs/ca_config.txt
+openssl req -new -x509 -key certs/ca.key -out certs/ca.crt -config certs/ca_config.cnf
 
 # CREATE THE PRIVATE KEY FOR OUR WEBHOOK
 openssl genrsa -out certs/app-validation-webhook-key.pem 2048
 
 # CREATE A CSR FROM THE CONFIGURATION FILE AND OUR PRIVATE KEY
-openssl req -new -key certs/app-validation-webhook-key.pem -subj "/CN=app-validation-webhook.default.svc" -out app-validation-webhook.csr -config certs/app-validation-webhook-config.txt
+openssl req -new -key certs/app-validation-webhook-key.pem -subj "/CN=app-validation-webhook.default.svc" -out certs/app-validation-webhook.csr -config certs/app-validation-webhook-config.cnf
 
 # CREATE THE CERT SIGNING THE CSR WITH THE CA CREATED BEFORE
-openssl x509 -req -in app-validation-webhook.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/app-validation-webhook-crt.pem
+openssl x509 -req -in certs/app-validation-webhook.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -extensions req_ext -extfile certs/app-validation-webhook-config.cnf -out certs/app-validation-webhook-crt.pem
